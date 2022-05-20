b43877f8b37185aa0fe8c14eb488041f7c46e71a
norhu1130 <norhu1130@naver.com>
2022-05-20 21:08:35 +0900

    Fixed Code Error

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -57,7 +57,7 @@ if(!isDev){
                 loggerAutoUpdaterSuccess.log('New update available', info.version)
                 
                 if(process.platform === 'darwin'){
-                    info.darwindownload = `https://github.com/karu724/BeeLauncher/releases/download/v${info.version}/Bee-Launcher-setup.dmg
+                    info.darwindownload = `https://github.com/karu724/BeeLauncher/releases/download/v${info.version}/Bee-Launcher-setup.dmg`
                     showUpdateUI(info)
                 }
                 
