f4ecbe2bd9527fa13a7641afa1dd01deb3012bf5
wavgado <wavgado@gmail.com>
2022-05-22 09:34:15 +0900

    💬 한글화

--- app/assets/js/scripts/settings.js
+++ app/assets/js/scripts/settings.js
@@ -1476,7 +1476,7 @@ function settingsUpdateButtonStatus(text, disabled = false, handler = null){
  */
 function populateSettingsUpdateInformation(data){
     if(data != null){
-        settingsUpdateTitle.innerHTML = `New ${isPrerelease(data.version) ? 'Pre-release' : 'Release'} Available`
+        settingsUpdateTitle.innerHTML = `새로운 ${isPrerelease(data.version) ? 'Pre-release' : 'Release'} 사용가능`
         settingsUpdateChangelogCont.style.display = null
         settingsUpdateChangelogTitle.innerHTML = data.releaseName
         settingsUpdateChangelogText.innerHTML = data.releaseNotes
